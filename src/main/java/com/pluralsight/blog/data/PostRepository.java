0cc55c4b37e84bf97c6b3421dac4f96b6816966b
pilot007 <selimgoktas@gmail.com>
2022-03-06 21:53:10 +0100

    Update PostRepository.java

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,4 +3,6 @@ package com.pluralsight.blog.data;
 import org.springframework.data.jpa.repository.JpaRepository; 
 import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+List<Post> findByTitleContaining(String title); 
+}
